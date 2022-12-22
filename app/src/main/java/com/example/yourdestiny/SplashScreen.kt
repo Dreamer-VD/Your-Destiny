56b0e5a22d725a6db60f333e436cc42bbb4b02ff
Dreamer-VD <danilavacun@gmail.com>
2022-12-22 13:53:18 +0300

    Fixed dialogues, fixed music, etc.

--- app/src/main/java/com/example/yourdestiny/SplashScreen.kt
+++ app/src/main/java/com/example/yourdestiny/SplashScreen.kt
@@ -1,6 +1,7 @@
 package com.example.yourdestiny
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,6 +10,9 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val intent:Intent=Intent(this,MainActivity::class.java)
         startActivity(intent)
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext,R.raw.music)
+        mediaPlayer.isLooping=true
+        mediaPlayer.start()
         finish()
     }
 }
